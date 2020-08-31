a84c4f6fa9fb1c166be2028d464a0b20a008d375
Kek01101 <67284308+Kek01101@users.noreply.github.com>
2020-08-31 12:48:34 +0100

    Add files via upload

--- spark_28-8.py
+++ spark_28-8.py
@@ -1,4 +1,13 @@
-#Spark of learning challenge from the 28th of August
+"""
+Problem category: Spark of Learning
+Problem definition: Create a short program to determine what
+                    drink someone is drinking based upon
+                    the e's in their name
+Attempted solutions: Just this one, it was easy enough
+Reflections: As was said in class, once you know what the
+             solution to the spark is, it's pretty easy to
+             implement.
+"""
 drinks = dict()
 people = ["Elizabeth", "Bert", "Dave", "Helen"]
 for person in people:
